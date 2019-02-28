e511df5e47948f37e58534cebc1aebe7ea74ac32
yadik64 <yadik64@gmail.com>
2019-02-28 21:04:16 +0400

    Без изменений

--- AuthorizationVK/AuthorizationVK/LoginFormViewController.swift
+++ AuthorizationVK/AuthorizationVK/LoginFormViewController.swift
@@ -10,21 +10,82 @@ import UIKit
 
 class LoginFormViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
-    */
-
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @IBAction func authorizationButton() {
+        
+        guard let login = self.loginTextField.text else {
+            return
+        }
+        guard let password = self.passwordTextField.text else {
+            return
+        }
+        
+        if login == "admin" && password == "123456" {
+            print("Доступ разрешен!")
+        } else {
+            createAlert(title: "Ошибка", message: "Неверный логин или пароль", style: .alert)
+        }
+        
+    }
+    
+    
+    func createAlert(title: String, message: String, style: UIAlertController.Style) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let actionOK = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+        alertController.addAction(actionOK)
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    @objc func keyboardWasShow(notification: Notification) {
+        
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        self.scrollView?.contentInset = contentInsets
+        self.scrollView?.scrollIndicatorInsets = contentInsets
+        
+    }
+    
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        
+        let contentInsets = UIEdgeInsets.zero
+        self.scrollView?.contentInset = contentInsets
+        self.scrollView?.scrollIndicatorInsets = contentInsets
+        
+    }
+    
+    @objc func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+    }
+    
 }
