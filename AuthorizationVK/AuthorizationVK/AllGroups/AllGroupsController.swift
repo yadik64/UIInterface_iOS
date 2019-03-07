c9120ab9ffe33eafbc2c0fb05d8520adf2dc44ad
yadik64 <yadik64@gmail.com>
2019-03-07 14:16:01 +0400

    Добавил группы. Возможность добавлять группы и удалять.

---
+++ AuthorizationVK/AuthorizationVK/AllGroups/AllGroupsController.swift
@@ -0,0 +1,63 @@
+//
+//  AllGroupsController.swift
+//  AuthorizationVK
+//
+//  Created by Дмитрий Яровой on 07/03/2019.
+//  Copyright © 2019 Дмитрий Яровой. All rights reserved.
+//
+
+import UIKit
+
+class AllGroupsController: UIViewController {
+    
+    let allGroups = [
+        (nameGroup: "iOS Developers", nameIcon: "appleIcon"),
+        (nameGroup: "Free Rider", nameIcon: "freeRiderIcon"),
+        (nameGroup: "Saratov News", nameIcon: "saratovNewsIcon"),
+        (nameGroup: "Tattoo", nameIcon: "tattooIcon"),
+        (nameGroup: "Group One", nameIcon: "anotherGroupIcon"),
+        (nameGroup: "Group Two", nameIcon: "anotherGroupIcon"),
+        (nameGroup: "Group Three", nameIcon: "anotherGroupIcon"),
+        (nameGroup: "Group Four", nameIcon: "anotherGroupIcon"),
+        (nameGroup: "Group Five", nameIcon: "anotherGroupIcon"),
+        (nameGroup: "Group News", nameIcon: "anotherGroupIcon"),
+        (nameGroup: "Group Six", nameIcon: "anotherGroupIcon"),
+        (nameGroup: "Group Seven", nameIcon: "anotherGroupIcon"),
+        (nameGroup: "Group Eight", nameIcon: "anotherGroupIcon"),
+        (nameGroup: "Group Nine", nameIcon: "anotherGroupIcon"),
+        (nameGroup: "Group Ten", nameIcon: "anotherGroupIcon")
+    ]
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchGroupBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
+extension AllGroupsController : UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allGroups.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let identifier = AllGroupsCell.className()
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! AllGroupsCell
+
+        let icon = UIImage(named: allGroups[indexPath.row].nameIcon)
+        let nameGroup = allGroups[indexPath.row].nameGroup
+
+        cell.nameIconImage.image = icon
+        cell.nameGroupLabel.text = nameGroup
+
+        return cell
+
+    }
+
+}
---
+++ AuthorizationVK/AuthorizationVK/MyGroups/UserGroupsController.swift
@@ -0,0 +1,99 @@
+//
+//  UserGroupsController.swift
+//  AuthorizationVK
+//
+//  Created by Дмитрий Яровой on 06/03/2019.
+//  Copyright © 2019 Дмитрий Яровой. All rights reserved.
+//
+
+import UIKit
+
+class UserGroupsController: UIViewController {
+    
+    var userGroupsArray = [
+        (nameGroup: "iOS Developers", nameIcon: "appleIcon"),
+        (nameGroup: "Free Rider", nameIcon: "freeRiderIcon"),
+        (nameGroup: "Saratov News", nameIcon: "saratovNewsIcon"),
+        (nameGroup: "Tattoo", nameIcon: "tattooIcon")
+    ]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func addGroup(segue: UIStoryboardSegue) {
+        
+        let segueIdetifier = "addGroup"
+        
+        guard segue.identifier == segueIdetifier else {
+            return
+        }
+        
+        let allGroupsController = segue.source as! AllGroupsController
+        
+        guard let indexPath = allGroupsController.tableView.indexPathForSelectedRow else {
+            return
+        }
+        
+        let city = allGroupsController.allGroups[indexPath.row]
+        
+        guard !userGroupsArray.contains(where: {$0 == city}) else {
+            return
+        }
+        
+        userGroupsArray.append(city)
+        tableView.reloadData()
+        
+    }
+
+}
+
+extension UserGroupsController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userGroupsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let identifier = UserGroupsCell.className()
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! UserGroupsCell
+        let icon = UIImage(named: userGroupsArray[indexPath.row].nameIcon)
+        let nameGroup = userGroupsArray[indexPath.row].nameGroup
+        cell.nameIconImage.image = icon
+        cell.nameGroupLabel.text = nameGroup
+        
+        return cell
+    }
+    
+    
+}
+
+extension UserGroupsController : UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            userGroupsArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+        
+    }
+    
+}
+
+extension NSObject {
+    
+    static func className() -> String {
+        
+        return String(describing: self)
+        
+    }
+    
+}
